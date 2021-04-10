01db53f54d99f5127d585673bfab96238b303e0c
Rahul Patil <71975383+rahulpatl@users.noreply.github.com>
2021-04-10 21:06:44 +0530

    Created Project
    Added project files.

---
+++ BMS Mobile Assignment/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  BMS Mobile Assignment
+//
+//  Created by Rahul Patil on 10/04/21.
+//
+
+import UIKit
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    
+    return true
+  }
+  
+
+}
+
---
+++ BMS Mobile Assignment/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BMS Mobile Assignment
+//
+//  Created by Rahul Patil on 10/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
